eadbeddd9020bff8b16a61885636db551a0b9274
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-12 10:44:53 -0600

    Update hobbit.js to pass test 7

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,12 +1,15 @@
 class Hobbit {
 	constructor(name, age = 0) {
 		this.name = name;
-		this.age = age
+		this.age = age;
+		this.adult = false;
 	}
 
 	celebrateBirthday(){
 		this.age += 1
 	}
+
+
 
 }
 module.exports = Hobbit;

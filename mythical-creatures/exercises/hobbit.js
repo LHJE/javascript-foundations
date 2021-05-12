876a5862f74745bdf29347f9bc8ef52d6a3282f7
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-12 10:46:46 -0600

    Update hobbit.js to pass test 9

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -3,12 +3,16 @@ class Hobbit {
 		this.name = name;
 		this.age = age;
 		this.adult = false;
+		this.old = false;
 	}
 
 	celebrateBirthday(){
 		this.age += 1
 		if(this.age >= 33) {
 			this.adult = true;
+		}
+		if(this.age >= 101) {
+			this.old = true;
 		}
 	}
 

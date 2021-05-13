2763257186ed82dd02d35532333284502bb03b1f
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-13 11:26:00 -0600

    Update centaur.js to pass tests 8, 9

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -4,14 +4,25 @@ class Centaur {
 		this.breed = data.type;
 		this.cranky = false;
 		this.standing = true;
+		this.actions = 0;
 	}
 
 	shoot(){
+		this.actions += 1;
+		this.crankyCheck();
 		return 'Twang!!!';
 	}
 
 	run(){
+		this.actions += 1;
+		this.crankyCheck();
 		return 'Clop clop clop clop!!!';
+	}
+
+	crankyCheck(){
+		if (this.actions >= 3) {
+			this.cranky = true
+		}
 	}
 }
 module.exports = Centaur;

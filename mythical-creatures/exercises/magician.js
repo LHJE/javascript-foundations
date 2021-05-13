9ac78881c1f0b37d0003445171a56c9d29be9f24
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-13 10:23:28 -0600

    Update magician.js to pass test 9

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,24 +2,30 @@ class Magician {
 	constructor(data) {
 		this.name = data.name;
 		this.confident = false;
+		this.spellsCast = 0;
 		if(data.topHat === undefined ){
 			this.topHat = true;
 		} else {
 			this.topHat = data.topHat;
-		}
+		};
 	}
 
 	incantation(spell){
-		return spell.toUpperCase() + "!"
+		return spell.toUpperCase() + "!";
 	}
 
 	cast(){
+		this.spellsCast += 1;
+		if(this.spellsCast >= 3){
+			this.confident = true
+		};
 		if(this.topHat){
 			return 'PULL RABBIT FROM TOP HAT'
 		} else {
 			return 'PULL DOVE FROM SLEEVE'
-		}
+		};
 	}
+
 }
 
 module.exports = Magician;

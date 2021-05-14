31e1ed6c209cbf9ae044b0155562ffd462b287c8
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 16:00:16 -0600

    Update golfer.js to pass test 9

--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -8,5 +8,13 @@ class Golfer {
 	calculateAvg(par){
 		return `I usually shoot a ${par + this.handicap} on average.`
 	}
+
+	playRound(golfCourse){
+		if (golfCourse.difficulty === 'hard') {
+			this.frustration += 500
+		} else {
+			this.frustration += 100
+		}
+	}
 }
 module.exports = Golfer;

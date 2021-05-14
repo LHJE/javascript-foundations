a1d1edb3644ff77bc34b00d79030f265aa27de68
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 10:18:40 -0600

    Update stark.js to pass direwolf test 15

--- mythical-creatures/exercises/stark.js
+++ mythical-creatures/exercises/stark.js
@@ -2,6 +2,11 @@ class Stark {
 	constructor(name, location = 'Winterfell') {
 		this.name = name;
 		this.location = location;
+		this.safe = false;
+	}
+
+	houseWords(){
+		return 'Winter is Coming';
 	}
 }
 

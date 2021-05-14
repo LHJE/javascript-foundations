8fea89ea777eca9779d0239517f5c2735ec2d7cc
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 12:35:24 -0600

    Update sphinx.js to pass test 8

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -14,6 +14,7 @@ class Sphinx {
 		for (let i = 0; i < this.riddles.length; i++){
 			if (guess === this.riddles[i]['answer']) {
 				this.riddles.splice(i, 1);
+				return 'That wasn\'t that hard, I bet you don\'t get the next one';
 			};
 		};
 	}

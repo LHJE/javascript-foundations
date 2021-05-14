6cd92115499d9034fd22713d0a7695d4a074f17b
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 16:01:37 -0600

    Update golfer.js to pass test 10

--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -3,18 +3,23 @@ class Golfer {
 		this.name = data['name'];
 		this.handicap = data['handicap'];
 		this.frustration = 0;
+		this.confidence = 0;
 	}
 
 	calculateAvg(par){
-		return `I usually shoot a ${par + this.handicap} on average.`
+		return `I usually shoot a ${par + this.handicap} on average.`;
 	}
 
 	playRound(golfCourse){
 		if (golfCourse.difficulty === 'hard') {
-			this.frustration += 500
+			this.frustration += 500;
 		} else {
-			this.frustration += 100
+			this.frustration += 100;
 		}
+	}
+
+	hitTheRange(){
+		this.confidence += 10;
 	}
 }
 module.exports = Golfer;

969e89b6936a6ef6c406b40de2b24642225a04a7
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 11:23:37 -0600

    Update ogre.js to pass tests 7, 8

--- mythical-creatures/exercises/human.js
+++ mythical-creatures/exercises/human.js
@@ -3,6 +3,15 @@ class Human {
 		this.name = name;
 		this.encounterCounter = 0;
 	}
+
+	noticesOgre(){
+		if (this.encounterCounter >= 3) {
+			this.encounterCounter = 0
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
 
 module.exports = Human;

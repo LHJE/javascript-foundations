dae19e1d937c6685e919ca383b243afbfe3ea2f6
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 10:08:38 -0600

    Update direwolf.js to pass tests 6-7

--- mythical-creatures/exercises/direwolf.js
+++ mythical-creatures/exercises/direwolf.js
@@ -1,7 +1,8 @@
 class Direwolf {
-	constructor(name, home = 'Beyond the Wall') {
+	constructor(name, home = 'Beyond the Wall', size = 'Massive') {
 		this.name = name;
 		this.home = home;
+		this.size = size
 	}
 }
 

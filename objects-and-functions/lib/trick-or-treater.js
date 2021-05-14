e445f46d1e70e9ed783def90b481e4f6b1e2b82d
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 14:26:10 -0600

    Update trick-or-treater.js to pass test 8

--- objects-and-functions/lib/trick-or-treater.js
+++ objects-and-functions/lib/trick-or-treater.js
@@ -3,12 +3,15 @@ class TrickOrTreater {
 		this.dressedUpAs = costume.style;
 		this.bag = bag;
 		this.hasCandy = false;
+		this.countCandies = 0;
 	}
 
 	putCandyInBag(candy){
 		this.bag.fill(candy);
 		this.hasCandy = true;
+		this.countCandies++;
 	}
+
 }
 
 module.exports = TrickOrTreater;

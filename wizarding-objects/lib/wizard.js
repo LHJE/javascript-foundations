ad75e848a22272cdd9ca4a1a774b93e57d5e14ea
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 15:43:43 -0600

    Update wizard.js to pass tests 4

--- wizarding-objects/lib/wizard.js
+++ wizarding-objects/lib/wizard.js
@@ -3,12 +3,30 @@ class Wizard {
 		this.name = data['name'];
 		this.pets = [];
 		this.petsCount = 0;
+		this.petSentence = '';
 	}
 
 	adoptPet(pet){
 		this.pets.push(pet);
 		this.petsCount++;
 	}
+
+	petList(){
+		var vowels = ['a', 'e', 'i', 'o', 'u'];
+		for (var i = 0; i < this.pets.length; i++) {
+			this.sentence(vowels, i, this.pets);
+		}
+		return this.petSentence.slice(2) + '.';
+	}
+
+	sentence(vowels, i, pets){
+		if (vowels.indexOf(pets[i].type[0].toLowerCase()) != -1) {
+			this.petSentence += `; An ${pets[i].type} named ${pets[i].name}`;
+		} else {
+			this.petSentence += `; A ${pets[i].type} named ${pets[i].name}`;
+		}
+	}
+
 }
 
 module.exports = Wizard;

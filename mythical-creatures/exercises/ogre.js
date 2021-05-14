abb571c848a2c339ba293deea6e2b7a9bd5c4d57
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 11:32:10 -0600

    Update ogre.js to pass test 9

--- mythical-creatures/exercises/ogre.js
+++ mythical-creatures/exercises/ogre.js
@@ -2,10 +2,15 @@ class Orge {
 	constructor(name, home = 'Swamp') {
 		this.name = name;
 		this.home = home;
+		this.swings = 0;
 	}
 
 	encounter(human){
 		human.encounterCounter += 1;
+	}
+
+	swingAt(human){
+		this.swings += 1;
 	}
 }
 

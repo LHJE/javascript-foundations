5475f044169225b84de5122f7d03f24b6b3922bb
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 15:15:53 -0600

    Update wizard.js to pass tests 2, 3

--- wizarding-objects/lib/wizard.js
+++ wizarding-objects/lib/wizard.js
@@ -1,6 +1,13 @@
 class Wizard {
 	constructor(data) {
 		this.name = data['name'];
+		this.pets = [];
+		this.petsCount = 0;
+	}
+
+	adoptPet(pet){
+		this.pets.push(pet);
+		this.petsCount++;
 	}
 }
 

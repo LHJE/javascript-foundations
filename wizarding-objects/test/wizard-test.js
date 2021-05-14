b04be20ae4d13dfe9cc34ca9f2e644c87e10b626
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 14:55:32 -0600

    Update repo with new exercises

--- wizarding-objects/test/wizard-test.js
+++ wizarding-objects/test/wizard-test.js
@@ -5,22 +5,16 @@ const Wizard = require('../lib/wizard')
 
 describe('Wizard', () => {
 
-  it('should be a function', () => {
-    const wizard = new Wizard();
-
-    assert.isFunction(wizard);
-  });
-
   it('should have a name', () => {
-    const hermyown = new Wizard('Hermione');
-    const wonwon = new Wizard('Ron');
+    const hermyown = new Wizard({ name: 'Hermione'});
+    const wonwon = new Wizard({name: 'Ron'});
 
     assert.equal(hermyown.name, 'Hermione');
     assert.equal(wonwon.name, 'Ron');
   });
 
   it('should have a pet', () => {
-    const wizard = new Wizard('Hermione');
+    const wizard = new Wizard({name: 'Hermione'});
     const pet = new Pet('Crookshanks', 'cat');
 
     wizard.adoptPet(pet);
@@ -30,11 +24,11 @@ describe('Wizard', () => {
   });
 
   it('should be able to have many pets', () => {
-    const wizard = new Wizard('Hermione');
- 
+    const wizard = new Wizard({name: 'Hermione'});
+
     const pet1 = new Pet('Crookshanks', 'cat');
     const pet2 = new Pet('Hedwig', 'owl');
-   
+
     wizard.adoptPet(pet1);
     wizard.adoptPet(pet2);
 
@@ -44,33 +38,33 @@ describe('Wizard', () => {
   });
 
   it('should list pets as a string', () => {
-    const wizard = new Wizard('Hermione');
-    
+    const wizard = new Wizard({name: 'Hermione'});
+
     const pet1 = new Pet('Crookshanks', 'cat');
     const pet2 = new Pet('Hedwig', 'owl');
-    
+
     wizard.adoptPet(pet1);
     wizard.adoptPet(pet2);
 
     var hermionesPets = wizard.petList();
 
-    assert.equal(hermionesPets, "A cat named Crookshanks; an owl named Hedwig.");
+    assert.equal(hermionesPets, "A cat named Crookshanks; An owl named Hedwig.");
   });
 
   it('should have a wand', () => {
-    const wizard = new Wizard('Hermione');
+    const wizard = new Wizard({name: 'Hermione'});
     const wand = new Wand("Cherry", 12, "Unicorn Hair");
- 
-    wizard.giveWand(wand);
+
+    wizard.getWand(wand);
 
     assert.equal(wizard.wand, wand);
   });
 
   it('should cast a spell', () => {
-    const wizard = new Wizard('Hermione');
+    const wizard = new Wizard({name: 'Hermione'});
     const wand = new Wand("Cherry", 12, "Unicorn Hair");
-   
-    wizard.giveWand(wand);
+
+    wizard.getWand(wand);
 
     assert.equal(wizard.wand, wand);
     assert.equal(wizard.castSpell('Expecto Patromun'), 'Casting Expecto Patromun!');

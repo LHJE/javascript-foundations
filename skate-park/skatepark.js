fbdb87c061117a250d4967ee974d5a90a114ad7d
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-14 17:01:32 -0600

    Update skatepark.js to pass tests 5-7

--- skate-park/skatepark.js
+++ skate-park/skatepark.js
@@ -3,6 +3,14 @@ class SkatePark {
 		this.name = data['name'];
 		this.yearFounded = data['year'];
 		this.style = data['type'];
+		this.features = data['features'];
+		if (typeof(data['isPrivate']) === 'undefined') {
+			this.isPrivate = false;
+			this.cost = 0;
+		} else {
+			this.isPrivate = data['isPrivate'];
+			this.cost = data['price'];
+		}
 	}
 }
 

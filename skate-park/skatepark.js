142cc98b9f775151668c8fb2df22049729a8a173
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-17 10:48:37 -0600

    Update skater.js to pass test 10

--- skate-park/skatepark.js
+++ skate-park/skatepark.js
@@ -15,17 +15,22 @@ class SkatePark {
 	}
 
 	admit(skater){
-		if (this.isPrivate) {
-			if (this.cost > skater.money) {
-				return `Sorry, you don't have enough money.`;
-			}
-			skater.money -= this.cost;
-			this.occupants.push(skater);
-			return `Welcome to ${this.name}, the cost will be $${this.cost}.00.`;
+		if (this.occupants.length >= 3) {
+			return 'Sorry, we are at max capacity. Thank you for understanding.';
 		} else {
-			this.occupants.push(skater);
-			return `Welcome to the free ${this.name} Skatepark!`;
-		}
+			if (this.isPrivate) {
+				if (this.cost > skater.money) {
+					return `Sorry, you don't have enough money.`;
+				} else {
+					skater.money -= this.cost;
+					this.occupants.push(skater);
+					return `Welcome to ${this.name}, the cost will be $${this.cost}.00.`;
+				};
+			} else {
+				this.occupants.push(skater);
+				return `Welcome to the free ${this.name} Skatepark!`;
+			};
+		};
 	}
 }
 

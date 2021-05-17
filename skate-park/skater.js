7366237053ec5643dafce96d4e876740b8751d20
Luke Hunter James-Erickson <theeluke@gmail.com>
2021-05-17 10:40:46 -0600

    Update skater.js to pass tests 18-20

--- skate-park/skater.js
+++ skate-park/skater.js
@@ -1,3 +1,4 @@
+
 class Skater {
 	constructor(data) {
 		this.name = data.name;
@@ -8,8 +9,13 @@ class Skater {
 	}
 
 	practice(trick){
-		if (!this.tricks[`${trick}`]) {
+		if (!this.tricks[trick]) {
 			this.frustration++;
+		}
+		if (this.frustration >= 3) {
+			this.tricks[trick] = true;
+			this.frustration = 0;
+			return `I just learned to ${trick}!!!`;
 		}
 	}
 }
